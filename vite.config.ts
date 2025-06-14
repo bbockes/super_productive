c58225c478ae77ef310b638ace954bdc9b32b52b
Brendan <59584595+bbockes@users.noreply.github.com>
2025-06-14 11:51:32 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/super_productive/', // Use only the repository name
+  base: '/super_productive/', // Ensure this matches your repository name
 })
