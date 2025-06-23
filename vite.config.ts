6d95734b955ba43439d22038fbf82c865a6759af
bbockes <59584595+bbockes@users.noreply.github.com>
2025-06-23 11:32:57 -0400

    Remove base path and redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/super_productive/', // Ensure this matches your repository name
   server: {
     proxy: {
       '/api': {
