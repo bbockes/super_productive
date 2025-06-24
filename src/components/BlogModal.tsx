5522c403bc2e8972387bd60183362ef5206f8f06
bbockes <59584595+bbockes@users.noreply.github.com>
2025-06-24 13:26:12 -0400

    Fix overlapping inline code highlights

--- src/components/BlogModal.tsx
+++ src/components/BlogModal.tsx
@@ -54,7 +54,7 @@ function InlineCodeBlock({ children }: { children: React.ReactNode }) {
           copied 
             ? 'bg-green-200 dark:bg-green-400 text-green-900 dark:text-gray-900' 
             : 'bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 group-hover:bg-gray-200 dark:group-hover:bg-gray-600'
-        }`}
+        } my-0.5`}
         onClick={handleCopy}
         style={{ fontFamily: 'inherit' }}
       >
