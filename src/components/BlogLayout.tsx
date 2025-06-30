746f4dfd46e6195f58b5378e6beb058340466743
bbockes <59584595+bbockes@users.noreply.github.com>
2025-06-30 12:26:34 -0400

    Increase link card size in App view

--- src/components/BlogLayout.tsx
+++ src/components/BlogLayout.tsx
@@ -584,7 +584,11 @@ export function BlogLayout() {
 
             {/* Content Grid */}
             {!(isLinkMode ? linkLoading : loading) && !(isLinkMode ? linkError : error) && (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
+              <div className={`grid gap-6 w-full ${
+                isLinkMode 
+                  ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3' 
+                  : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+              }`}>
                 {isLinkMode ? (
                   filteredPosts.map((linkCard: any) => (
                     <LinkCard key={linkCard._id} linkCard={linkCard} />
